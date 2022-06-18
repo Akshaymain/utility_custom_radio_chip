6bd714c3c3f5943da33f06c4c4cc61230a660c65
Akshay Kumar <akshaymain95@gmail.com>
2022-06-18 13:27:41 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/utility_custom_radio_chip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.utility_custom_radio_chip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/custom_radio_chip.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:utility_custom_radio_chip/constants/colors.dart';
+
+enum ChipType { text, check }
+
+class CustomChips extends StatefulWidget {
+  final ChipType chipType;
+  final List<dynamic> chipItems;
+
+  const CustomChips({Key? key, required this.chipType, required this.chipItems})
+      : super(key: key);
+
+  @override
+  CustomChipsState createState() => CustomChipsState();
+}
+
+class CustomChipsState extends State<CustomChips> {
+  late List<bool> chipClicked;
+
+  @override
+  void initState() {
+    super.initState();
+    chipClicked = List<bool>.filled(widget.chipItems.length, false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: widget.chipItems
+            .asMap()
+            .entries
+            .map((element) => InkWell(
+                  onTap: () {
+                    setState(() {
+                      chipClicked.fillRange(0, widget.chipItems.length, false);
+                      chipClicked[element.key] = true;
+                    });
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        border: chipClicked[element.key]
+                            ? Border.all(width: 5, color: WHITE)
+                            : null,
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: widget.chipType == ChipType.check
+                            ? element.value
+                            : Theme.of(context).colorScheme.secondaryContainer,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withOpacity(
+                                      chipClicked[element.key] ? 0.2 : 0.4),
+                              offset: const Offset(0, 2),
+                              blurRadius: chipClicked[element.key] ? 2.0 : 0.75,
+                              spreadRadius:
+                                  chipClicked[element.key] ? 2.0 : 0.75)
+                        ]),
+                    child: widget.chipType == ChipType.check
+                        ? (chipClicked[element.key]
+                            ? const Icon(Icons.done)
+                            : const SizedBox.shrink())
+                        : widget.chipType == ChipType.text
+                            ? Center(
+                                child: Text(
+                                  element.value,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1!
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            : const SizedBox.shrink(),
+                  ),
+                ))
+            .toList());
+  }
+}
